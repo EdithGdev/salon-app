bdb2667cc59c5c106ab5bff7a6057cb1b51d3d90
EdithGdev <edithgonzalez9630@gmail.com>
2022-02-21 19:46:58 -0800

    working on hamburger

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import './App.css';
+import React from 'react';
 
 function App() {
+
+  const hamburger = document.querySelector('.hamburger')
+  const menu = document.querySelector('.menu')
+  console.log(hamburger)
+  hamburger.addEventListener('click', () =>{
+    hamburger.classList.toggle('active');
+    menu.classList.toggle('active');
+  })
+  
   return (
     <div className="app">
       <nav className='header'>
         <a className='logo' href='#' ><h3>SLAYED.BY SUZY STUDIO</h3></a>
-        <div className='hamburger'></div>
+        <ul className='menu'>
+          <li className='nav-item'>
+            <a href='#' className='nav-link'>Home</a>
+          </li>
+          <li className='nav-item'>
+            <a href='#' className='nav-link'>About</a>
+          </li>
+          <li className='nav-item'>
+            <a href='#' className='nav-link'>Contact</a>
+          </li>
+          
+        </ul>
+        <div className='hamburger'>
+          <span className='bar'></span>
+          <span className='bar'></span>
+          <span className='bar'></span>
+        </div>
       </nav>
       <div className='specialty-info'>
         <div className='bridal-party-info'>
