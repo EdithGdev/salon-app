01bee6071e2f7d87ffcdb5d906a55d8fb200e845
EdithGdev <edithgonzalez9630@gmail.com>
2022-03-06 16:02:38 -0800

    buttons increase and decrease count

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,19 +1,29 @@
 import React, {useState} from 'react'
 import { SliderData } from './SliderData'
-//UNCOMMENT 3/4/22
-// https://www.youtube.com/watch?v=l1MYfu5YWHc&t=31s&ab_channel=BrianDesign
-// 10:03
 import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from 'react-icons/fa'
 
 const ImageSlider = ({slides}) => {
 const [current, setCurrent] = useState(0)
 const length = slides.length
 
+const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1)
+}
+
+const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current -1)
+}
+
+console.log(current)
+
+if(!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+}
 
 return (
 <section className='slider'>
-    <FaArrowAltCircleLeft className="left-arrow" />
-    <FaArrowAltCircleRight className="right-arrow" />
+    <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide}/>
+    <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide}/>
 {SliderData.map((slide, index) => {
     return(
         <img src={slide.image} alt ='hair' className='image' />
